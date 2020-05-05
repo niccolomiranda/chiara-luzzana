8a7d1a9d66772f5749f2a372b5c641bb8153f6d2
marius ballot <ballot.marius@gmail.com>
2020-05-05 20:26:27 +0200

    sound debug

--- jsSnippets/webglBall7/index.js
+++ jsSnippets/webglBall7/index.js
@@ -204,6 +204,7 @@ class ThreeScene {
             child.material.uniforms.u_color.value = colorPool[n]
 
 
+            console.log(soundReactor)
             if (soundReactor.audio == undefined)
                 return
             child.material.uniforms.u_nDet.value += soundReactor.fdata[10] * GUIObj.lowFreqIntensity
